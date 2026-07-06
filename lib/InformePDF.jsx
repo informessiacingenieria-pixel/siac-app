0833d046f0930f3fb81b802ee35b89f6f1e23ac5
Valentina Gonzalez <vpgonzalez@miuandes.cl>
2026-07-06 16:38:20 -0400

    fix ruta import informesConfig en InformePDF

--- lib/InformePDF.jsx
+++ lib/InformePDF.jsx
@@ -1,5 +1,5 @@
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib'
-import { FIRMAS, formatearFechaLarga, formatearFechaCorta, calcularHoraTermino, PUNTOS_SIN_REUSO } from './informesConfig'
+import { FIRMAS, formatearFechaLarga, formatearFechaCorta, calcularHoraTermino, PUNTOS_SIN_REUSO } from '../lib/informesConfig'
 
 const PAGE_HEIGHT = 792
 
