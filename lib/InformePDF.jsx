5f42f20d8b86906076030441bd29f658ace986d9
Valentina Gonzalez <vpgonzalez@miuandes.cl>
2026-07-07 21:56:03 -0400

    ajuste firma nombre

--- lib/InformePDF.jsx
+++ lib/InformePDF.jsx
@@ -151,8 +151,8 @@ export async function generarPdfBlob(datos) {
     tiempoEnjuague: 480,
     puntosAusenciaTop: 505, puntosAusenciaBottom: 573.3,
     horaTermino: 570,
-    xFirma: 235, yFirma: 685,
-    xNombreCentro: 295, yNombre: 710,
+    xFirma: 245, yFirma: 650,
+    xNombreCentro: 300, yNombre: 700,
   } : {
     fechaInformeX: 405, fechaInformeY: 101.0,
     clienteX: 95, clienteY: 114.5,
