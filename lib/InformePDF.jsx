d3fa5542abc9fe5674e404c62ea86d50ac7b8a89
Valentina Gonzalez <vpgonzalez@miuandes.cl>
2026-07-07 22:09:47 -0400

    bajar firma y nombre

--- lib/InformePDF.jsx
+++ lib/InformePDF.jsx
@@ -151,8 +151,8 @@ export async function generarPdfBlob(datos) {
     tiempoEnjuague: 480,
     puntosAusenciaTop: 505, puntosAusenciaBottom: 573.3,
     horaTermino: 570,
-    xFirma: 245, yFirma: 665,
-    xNombreCentro: 305, yNombre: 690,
+    xFirma: 245, yFirma: 690,
+    xNombreCentro: 305, yNombre: 715,
   } : {
     fechaInformeX: 405, fechaInformeY: 101.0,
     clienteX: 95, clienteY: 114.5,
